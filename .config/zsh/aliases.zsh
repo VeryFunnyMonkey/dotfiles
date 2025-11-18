86bc762f4916759aa1f909f53f46a848ebffa809
VeryFunnyMonkey <62693226+VeryFunnyMonkey@users.noreply.github.com>
2025-11-18 13:27:04 +0800

    Add OS-specific update aliases in aliases.zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,3 +1,25 @@
 # shell aliases
 alias ls="ls --color=auto"
-alias update="yay -Syu"
+
+# Aliases based on MY_OS
+case "$MY_OS" in
+  macos)
+    alias update="brew update && brew upgrade"
+    ;;
+
+  arch)
+    alias update="yay -Syu"
+    ;;
+
+  fedora)
+    alias update="sudo dnf upgrade --refresh"
+    ;;
+
+  debian)
+    alias update="sudo apt update && sudo apt upgrade"
+    ;;
+
+  *)
+    alias update="echo 'No update command for $MY_OS'"
+    ;;
+esac
